0590fc665469b6ded62e80dde3510d70544d42ba
Billy <william.allan@glasgow.ac.uk>
2018-11-28 09:54:02 +0000

    Updated routes with a default '/' view route

--- src/stubs/routes/web.php
+++ src/stubs/routes/web.php
@@ -4,6 +4,8 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
+    Route::view('/', 'welcome');
+
     Route::impersonate(); // https: //github.com/404labfr/laravel-impersonate
 
     Route::group(['middleware' => 'admin', 'prefix' => '/admin'], function () {
